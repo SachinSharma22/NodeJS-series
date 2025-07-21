87ed03fa9ce97a3b3f1341feaf0813c288a2d547
SachinSharma22 <s830sharma@gmail.com>
2025-07-21 16:35:05 +0530

    create file

--- form.js
+++ form.js
@@ -1,40 +1,56 @@
 const http = require("http");
 const fs = require("fs");
-// http.createServer((req,res)=> {
-//     res.writeHead(200,{"content-type":"text/html"})
-//    if(req.url == '/'){
-//      res.write(`
-//         <form action="/submit" method="post">
-//         <input type="text" placeholder="Enter name" name='name'><br><br>
-//         <input type="text" placeholder="Enter email" name='email'><br><br>
-//         <button>Submit</button>
-//         </form>
-//         `);
-//    }
-//    else if(req.url == '/submit'){
-//     res.write('<h1>Data Submitted</h1>')
-//    }
-//     res.end()
-// }).listen(3300)
+const querystring = require("querystring");
 
-http
-  .createServer((req, res) => {
+http.createServer((req, res) => {
+  if (req.url === "/") {
     fs.readFile("HTML/form.html", "utf-8", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
         res.end("Internal Server Error");
-        res.end();
-        return
+        return;
       }
-      if (req.url == "/") {
-         res.writeHead(200,{"Content-Type":"text/html"})
-        res.write(data);
-        res.end();
-      } else if (req.url == "/submit") {
-        res.write("<h1>Data Submitted</h1>");
-      }
-        
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
     });
-   
-  })
-  .listen(3300);
+  }
+
+  else if (req.url === "/submit" && req.method === "POST") {
+    let dataBody = [];
+
+    req.on("data", (chunk) => {
+      dataBody.push(chunk);
+    });
+
+    req.on("end", () => {
+      const rawData = Buffer.concat(dataBody).toString();
+      const readableData = querystring.parse(rawData);
+      let dataString = "My name is "+readableData.name + " and my email id is "+readableData.email
+      // fs.writeFileSync('text/' +readableData.name+".txt", dataString)
+      // console.log(readableData);
+
+      fs.writeFile("text/" + readableData.name + ".txt", dataString,(err)=> {
+        if(err){
+          res.end("Internal server error")
+          return false
+        }else{
+          console.log("file created")
+        }
+      })
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Data Submitted</h1>");
+      res.write(`<h3>Name: ${readableData.name}</h3>`);
+      res.write(`<h3>Email: ${readableData.email}</h3>`);
+      res.end();
+    });
+  }
+
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page not found");
+  }
+}).listen(3300, () => {
+  console.log("Server running on http://localhost:3300");
+});
